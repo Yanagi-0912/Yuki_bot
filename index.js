7cb85f6571005a6fd810bb79f06bf15e15a985a3
Jeffery Yu <930912jeffery@gmail.com>
2024-11-26 03:59:06 +0800

    初始化機器人並確認可執行!hello指令

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates, // Add intent for voice state
+    ],
+});
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+    if(message.content === "!hello"){
+        message.channel.send("Hello!");
+    }
+})
+
+client.login(token);
